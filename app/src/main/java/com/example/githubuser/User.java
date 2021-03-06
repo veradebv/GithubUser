dfd9b95969402ec61cb7a835e8ac44f4bdd41067
Vera Debora Vitamas <veradebora@outlook.co.id>
2021-03-06 23:42:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/githubuser/User.java
@@ -0,0 +1,31 @@
+package com.example.githubuser;
+
+public class User {
+    private int photo;
+    private String name;
+    private String username;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ app/src/main/java/com/example/githubuser/UserList.java
@@ -0,0 +1,77 @@
+package com.example.githubuser;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class UserList extends RecyclerView.Adapter<UserList.ListViewHolder> {
+    private ArrayList<User> listUser;
+
+    public  UserList(ArrayList<User> list) {
+        this.listUser = list;
+    }
+
+    private OnItemClickCallback onItemClickCallback;
+
+    public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @NonNull
+    @Override
+    public UserList.ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_user, viewGroup, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserList.ListViewHolder holder, int position) {
+        User user = listUser.get(position);
+        Glide.with(holder.itemView.getContext())
+                .load(user.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+        holder.tvName.setText(user.getName());
+        holder.tvUsername.setText(user.getUsername());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickCallback.onItemClicked(listUser.get(holder.getAdapterPosition()));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listUser.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvUsername;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvUsername = itemView.findViewById(R.id.tv_item_username);
+        }
+    }
+
+    private class OnItemClickCallback {
+        void onItemClicked(User data);
+    }
+
+}
